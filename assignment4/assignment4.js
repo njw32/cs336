bc680ca0b4a669651d6b8d30eb04d9906838451d
unknown <njw32@students.calvin.edu>
2020-10-02 22:49:14 -0400

    assignment4 complete

--- assignment4/assignment4.js
+++ assignment4/assignment4.js
@@ -1,8 +1,18 @@
+//variables declaration
 const textArea = document.getElementById("textArea");
 const words = document.getElementById("numberOfWords");
 const chars = document.getElementById("numberOfCharacters");
 
 
 document.addEventListener("keyup", () => {
-  chars.innerHTML = textArea.innerHTML.length;
+  //ternary plural variable for characters - write to html
+  plurchars = textArea.value.length != 1 ? " characters" : " character";
+  chars.innerHTML = textArea.value.length + plurchars;
+
+  //ternary plural variable for words - write to html
+  plurwords = textArea.value.split(" ").length - 1 != 1 ? " words" : " word";
+  words.innerHTML = textArea.value.split(" ").length - 1 + plurwords;
+
+  //convert to uppercase
+  textArea.value = textArea.value.toUpperCase();
 });
