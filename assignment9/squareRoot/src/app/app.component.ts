24147bed8ffaa58344b38b115bc2f52d413d46c8
unknown <njw32@students.calvin.edu>
2020-10-29 16:10:55 -0400

    mostly done

--- assignment9/squareRoot/src/app/app.component.ts
+++ assignment9/squareRoot/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ɵclearResolutionOfComponentResourcesQueue } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'squareRoot';
+  bigNumber: string = '16';
+  guess: string = '1';
+  responses: string[] = [];
+  submit = () => {
+    let guessNum: number = +this.guess;
+    let root: number = +this.bigNumber;
+    let correct = Math.sqrt(root);
+    let feedback: string;
+    if (Math.abs(correct - guessNum) < correct * .01) {
+      feedback = "You got it! The correct number was " + correct + ". It only took " + (this.responses.length + 1) + " tries.";
+    }
+    else if (guessNum > correct) {
+      feedback = guessNum + " is too high. Try again";
+    }
+    else {
+      feedback = guessNum + " is too low. Try again";
+    }
+    this.responses.push(feedback);
+    console.log(feedback);
+  };
+  randomNum = () => {
+    this.bigNumber = (Math.floor(Math.random() * 250) + 50).toString();
+
+  }
 }
+
