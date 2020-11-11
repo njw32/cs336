a0a60fa2ec64d74442791576143fbca4ce4476b1
unknown <njw32@students.calvin.edu>
2020-11-10 20:43:10 -0500

    created three cards

--- assignment10/hmwk10/src/app/app.module.ts
+++ assignment10/hmwk10/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { SurveyCardComponent } from './components/survey-card/survey-card.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SurveyCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatCardModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ assignment10/hmwk10/src/app/components/survey-card/survey-card.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-survey-card',
+  templateUrl: './survey-card.component.html',
+  styleUrls: ['./survey-card.component.scss']
+})
+export class SurveyCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  @Input() question: string;
+
+  @Input() min: number = 1;
+
+  @Input() max: number = 100;
+
+  @Input() units: string = '';
+
+}
+
