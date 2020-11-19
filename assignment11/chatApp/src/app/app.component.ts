53ee16d243adcb1b43e1afcb235490f76a0ef4ab
unknown <njw32@students.calvin.edu>
2020-11-18 23:25:01 -0500

    to do notes

--- assignment11/chatApp/src/app/app.component.ts
+++ assignment11/chatApp/src/app/app.component.ts
@@ -2,6 +2,14 @@ import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
+// to do:
+// interface for incoming collection
+// change valuechanges to subscribe
+// write to db
+// order messages
+// asssure local storage is functional
+// get messages color coded
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
