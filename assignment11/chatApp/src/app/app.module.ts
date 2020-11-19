12ae2dde2940288727fcef6acfe155ee7f7669f8
unknown <njw32@students.calvin.edu>
2020-11-18 22:09:17 -0500

    writing to html complete

--- assignment11/chatApp/src/app/app.module.ts
+++ assignment11/chatApp/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { firebaseConfig } from './credentials';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ assignment11/chatApp/src/app/credentials.js
@@ -0,0 +1,20 @@
+export const firebaseConfig = {
+  // type: "service_account",
+  // project_id: "chat-app-c2d2b",
+  // private_key_id: "162712f4f239083e8862a99d7e7dd8b5eb5cd8b5",
+  // private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDfpAmHetK+A/Fr\nn5SnRI1rYk/qwNMXKz7MmEaCzWemFkH4d2W3faw/BI7mmBawXk3xw16NaVRCMJqi\nwV9Edo1tRXAOITV3gq+hZ5XuhjQMRPaGZaAw88ugyLuBb0+1KbFC39c2QoS7ZwTA\nP/uMo6O8L5Z+5MOkcBP5sDZGE3wXRIYjVQlWRjGvyCoee89wb8p/jHkHlSaomwKi\nR3PIAe6y7QzUeeZR/duOmQK70V25G2542H7UzJIZtPrAdKZwaUM2rLxddFtzPGjE\nYrbj/8st67cJ/EE2j8mSZq2OZ4QWfvXo+Jtyq1FXNwpPJnCHlDWq2F6AgyARUcnE\niUOUsY/nAgMBAAECggEADJxT2zteSc+wG9j+SWZQsSb0ibDA1nQeUOwfQb3yY8eq\nhae56Rxhi5YmtrMJyH0w2PqiyMeRNEBIX/gWdqZWuDwrouhVev04CRpttmsD4VM8\nMXzmiQweRd070YMk0/rbX4F+wbUDhgu7lVmmRhpuH6zxSAt/SrT1M2Wv0cjEfQ1J\nm1wuyD659IMz10klBcWo3rwstVYYxbmI29vsu/NQIr5YVC8O3pEBCXFNpAUjSYyO\n6ZDU4tfQqSAm53GSpHZwzCpm79NX7qboyqZbEOV1LshIpi4+o6hRq9at/OkE6eqm\nl2sJjnp2YIeqrkRfGJaECR0LNdR1GTy0GgpEhZ/iJQKBgQD8OtNU8KvZ80Rncgv9\nLMRiPLjlUs7xi+bTf4LkXQS0+X0PlFc1eLvFG5ewlE4Ugg8IO9J7dUjzdlLfN1nh\nM0ZXELfx6Cxayzga8WWwljjm1KC/WEEma1heeOemzY2IA0UcpDN3Tj0GsZR4g6Nx\nqjoHio9+6pe/jKOxI6sDR2Kg8wKBgQDi+9Ba1NIg7K72a/G46oCEZLK17S0j6PhP\nCykaqotVf8TGYkuO9x+vr4+sDILz9gvHUQbQKv2t4epdyfsSaC8hldqHdHcXNIOq\nyPfZSxLYR1GWgY79bbwfnYHPMJbNN1Aem0ifuZwgQI/ivt2dXXc7WS/yeKynpgyG\n7fGkuKdyPQKBgQD2FGJINYJEYl1zW0YtHDcA7OMrAQ5bhLWP1JCcQdAszFfiEkS/\nK2puVIw6v4Yp5fXKf6rLvlKNDPyvQC5VUxQmL252+aAEeM3lI2ylyvn64qlt5Smy\n1PnjOawwBc02TJJp7h00IcXSOQB0T2HvrDdvUhTLqw6f2BN/6B+ZVFT6VwKBgFXf\nYrIadezS3laH/AkzXq+30QyW0E8Xoml1FXXrM4+DKRTMtz09LRWwW7CBg0F8wnQD\nqYoz2YlhQON+uKf8FmikYuqAG1YCHhYMtgfVKSn9wVYpacljLJrhYezauxKkbIFH\nyi/zbqedBVKJVxpj8+nWEhevo9JBNdgOSS9CVMcZAoGAIbLDrovnRvLozvGmx3AY\nsTpmkV6Bt4dJmwHNZDnBTXv0W8F1J/DnRZgZ4S1Xc2SaBRPRjIbKasmkUYl00Nol\nnV2Na3nSKSaEZPgqPSZPlsqBDjDh2NDUHKBFYAfSdPaJetpBXYJHDkxc8XA7LDjR\n5iQJq6H2goO2AItR1gSXjxw=\n-----END PRIVATE KEY-----\n",
+  // client_email: "firebase-adminsdk-af1nf@chat-app-c2d2b.iam.gserviceaccount.com",
+  // client_id: "100155238603865998912",
+  // auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  // token_uri: "https://oauth2.googleapis.com/token",
+  // auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  // client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-af1nf%40chat-app-c2d2b.iam.gserviceaccount.com"
+  apiKey: "AIzaSyDpjfSLLqjB8DwmWflp4yciXzy_x5ECNUM",
+  authDomain: "chat-app-c2d2b.firebaseapp.com",
+  databaseURL: "https://chat-app-c2d2b.firebaseio.com",
+  projectId: "chat-app-c2d2b",
+  storageBucket: "chat-app-c2d2b.appspot.com",
+  messagingSenderId: "24287544549",
+  appId: "1:24287544549:web:929c86d52817e629b0c00c",
+  measurementId: "G-G9CYB01RBN"
+}
